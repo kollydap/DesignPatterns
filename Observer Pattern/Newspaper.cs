62af9ecbfebadb444ddccc5ed8e45973f509afd4
Oladapo Kolawole Osagie <dapkolly@gmail.com>
2023-04-08 16:44:48 +0100

    another one

---
+++ Observer Pattern/Newspaper.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Observer_Pattern
+{
+    interface INewsPaperHub
+    {
+        void AddSuscribers(ISuscribers suscriber);
+        void RemoveSuscribers(ISuscribers suscriber);
+        void NotifySuscribers(); 
+    }
+    interface ISuscribers
+    {
+        void update(string title, string body, int refNo);
+    }
+
+    class Newspaper : INewsPaperHub
+    {
+        public int RefNo { get; set; }
+        public string  Title { get; set; }
+        public string Body { get; set; }
+        public Newspaper(string title, string body, int refNo)
+        {
+            RefNo = refNo;
+            Title = title;
+            Body = body;
+            NotifySuscribers();
+        }
+        List<ISuscribers> suscribersList = new List<ISuscribers>();
+        public void AddSuscribers(ISuscribers suscriber)
+        {
+            suscribersList.Add(suscriber); 
+
+        }
+        public void RemoveSuscribers(ISuscribers suscriber)
+        {
+            if (suscribersList.Count < 1) return;
+            suscribersList.Remove(suscriber);
+        }
+        public void UpdateSuscribers(string title, string body, int refNo)
+        {
+            RefNo = refNo;
+            Title = title;
+            Body = body;
+            NotifySuscribers();
+        }
+        public void  ClearAllSuscribers()
+        {
+            if (suscribersList.Count < 1) return;
+            suscribersList.Clear();
+        }
+        public void NotifySuscribers()
+        {
+            foreach(ISuscribers suscribers in suscribersList)
+            {
+                suscribers.update(Title,Body,RefNo);
+            }
+        }
+    }
+    //Later We will create a suscriber Clss that they must all inherit
+
+   public class Suscribers : ISuscribers
+    {
+        public int RefNo { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public void update(string title, string body, int refNo)
+        {
+            RefNo = refNo;
+            Title = title;
+            Body = body;
+            display();
+
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"{Title} \n {Body} \n {RefNo} ");
+        }
+
+    }
+    class KolawoleSuscriber : ISuscribers
+    {
+        public int RefNo { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public void update(string title, string body, int refNo)
+        {
+            RefNo = refNo;
+            Title = title;
+            Body = body;
+            display();
+
+        } 
+
+        public void display()
+        {
+            Console.WriteLine($"{Title} \n {Body} \n {RefNo} ");
+        }
+    }
+
+    class DupeSuscriber : ISuscribers
+    {
+        public int RefNo { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public void update(string title, string body, int refNo)
+        {
+            RefNo = refNo;
+            Title = title;
+            Body = body;
+            display();
+
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"{Title} \n {Body} \n {RefNo} ");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,15 +12,28 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
-            PlayerCharacter killerGuy = new PlayerCharacter();
-            Gunman gunman = new Gunman();
-            ArrowMan man = new ArrowMan();
-            man.DoStrike();
-            Distributor distributor = new Distributor();
-            Collectors collectors = new Collectors();
-            distributor.AddCollector(collectors);
-            distributor.NotifyCollectors();
+            //PlayerCharacter killerGuy = new PlayerCharacter();
+            //Gunman gunman = new Gunman();
+            //ArrowMan man = new ArrowMan();
+            //man.DoStrike();
+            //Distributor distributor = new Distributor();
+            //Collectors collectors = new Collectors();
+            //distributor.AddCollector(collectors);
+            //distributor.NotifyCollectors();
 
+            Newspaper newspaper = new Newspaper("kolawole Is Happy", "BODY............", 123);
+            Suscribers suscriber1 = new Suscribers();
+            Suscribers suscriber2 = new Suscribers();
+            Suscribers suscriber3 = new Suscribers();
+            Suscribers suscriber4 = new Suscribers();
+            //KolawoleSuscriber kolawoleSuscriber = new KolawoleSuscriber();
+            //DupeSuscriber dupeSuscriber = new DupeSuscriber();
+            newspaper.AddSuscribers(suscriber1);
+            newspaper.AddSuscribers(suscriber2);
+            newspaper.AddSuscribers(suscriber3);
+            newspaper.AddSuscribers(suscriber4);
+            //newspaper.AddSuscribers(kolawoleSuscriber);
+            newspaper.UpdateSuscribers("helooooo", "kilooo", 583);
             Console.ReadLine();
         }
     }
