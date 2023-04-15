817a2c9b1e873eb30985c3ac3890b28280351669
Oladapo Kolawole Osagie <dapkolly@gmail.com>
2023-04-15 12:46:04 +0100

    Another Commit

---
+++ Factory Pattern/DominoPizza.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Factory_Pattern
+{
+    public class Pizza
+    {
+
+    }
+    public class DominoPizza
+    {
+        SimplePizzaFactory factory;
+        public DominoPizza(SimplePizzaFactory factory)
+        {
+            this.factory = factory;
+        }
+        public DominoPizza orderPizza(String type)
+        {
+            DominoPizza pizza;
+            pizza = factory.CreatePizza(type);
+            //pizza.prepare();
+            //pizza.bake();
+            //pizza.cut();
+            //pizza.box();
+            return pizza;
+        }
+        public class SimplePizzaFactory
+        {
+            public DominoPizza CreatePizza(String type)
+            {
+                DominoPizza pizza = null;
+                if (type == "cheese")
+                {
+                    //pizza = new CheesePizza();
+                    Console.WriteLine("cREATE JDJ");
+                    return pizza;
+                }
+                else if (type == "pepperoni")
+                {
+                    //pizza = new PepperoniPizza();
+                    Console.WriteLine("cREATE JDJ");
+                    return pizza;
+                }
+                else
+                {
+                    return pizza;
+                }
+
+            }
+        }
+    }
+}
